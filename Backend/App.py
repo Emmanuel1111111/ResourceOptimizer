ec82b8e0b27f563a82d94f4df14a4f6a4856ea6d
Emmanuel1111111 <asante1036@gmail.com>
2025-10-25 19:24:14 +0000

    Okayy

--- Backend/App.py
+++ Backend/App.py
@@ -32,7 +32,7 @@ setup_logging(app)
 if os.getenv('FLASK_ENV') == 'production':
     CORS(app,
      origins=[
-         "https://resource-optimizer-01.vercel.app",
+         "https://resource-optimizer03.vercel.app",
          "https://resource-optimizer-ikux.vercel.app",
          "http://localhost:4200",
          "http://localhost:54119"
